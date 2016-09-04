954dd1f4c33587df782c1478994a0959b2061ea7
JennyShalai <jenny.shalai@gmail.com>
2016-09-04 14:44:00 -0400

    form dictionary shortest-Meeting-For-Each-Start

--- zombit-antidote/main.swift
+++ zombit-antidote/main.swift
@@ -8,5 +8,40 @@
 
 import Foundation
 
-print("Hello, World!")
+func answer(input: [[Int]]) -> Int {
+    
+    var shortestMeetingForEachStart: [Int: [Int]] = [:]
+    
+    for meeting in input {
+        // make a dictionary where key is the beginning of the section and
+        // value is the section itself (which is element of the input list)
+        // going through all sections and choose only the smallest one for each beginning
+        // so for input like  [[1, 3], [3, 4], [2, 3], [2, 4]]
+        // dictionary will be [2: [2, 3], 3: [3, 4], 1: [1, 3]]
+        if let meetingStart = meeting.first {
+            if shortestMeetingForEachStart[meetingStart] == nil {
+                shortestMeetingForEachStart[meetingStart] = meeting
+            } else {
+                if let meetingStop = meeting.last {
+                    if shortestMeetingForEachStart[meetingStart]!.last! > meetingStop {
+                        shortestMeetingForEachStart[meetingStart] = meeting
+                    }
+                }
+            }
+        }
+    }
+    
+    print(shortestMeetingForEachStart)
+    
+    
+    
+    
+    
+    
+    return 0
+}
+
+let input1 = [[1, 3], [3, 4], [2, 3], [2, 4]]
+
+print(answer(input1))
 
